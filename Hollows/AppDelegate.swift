30a00270e835921e005d683d181dbdd69a1bee1e
TanmoyPanigrahi <tanmoy.panigrahi2@gmail.com>
2019-07-17 16:52:59 +0530

    added screends and design

--- Hollows/AppDelegate.swift
+++ Hollows/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        let navigationFont = UIFont(name: "Noteworthy-Light", size: 18)
+//        let navigationFontAttributes = [NSAttributedString.Key.font : navigationFont]
+//        UINavigationBar.appearance().titleTextAttributes = navigationFontAttributes
         return true
     }
 
